e1381846ff28bf46619bd28efa3ef26cb3c06880
SD-PARK <tkdeh129@naver.com>
2022-09-15 16:20:20 +0900

    Room 구현

--- app.js
+++ app.js
@@ -13,20 +13,30 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
     // 클라이언트 접속 시
     socket.on('login', (data) => {
-        console.log('client logged-in:\n name: ' + data.name + '\n userid: ' + data.userid);
+        console.log('Client logged-in:\n Name: ' + data.name + '\n Userid: ' + data.userid);
         socket.name = data.name;
         socket.userid = data.userid;
-
-        io.emit('login', socket.name);
     });
 
     // 클라이언트 채팅 입력 시
     socket.on('chat', (msg) => {
         console.log(socket.name + ': ' + msg);
-        io.emit('chat', {
+        io.to(socket.room).emit('chat', {
             name: socket.name,
             msg: msg
         });
+    });
+
+    // 방 입장 시
+    socket.on('joinRoom', (roomNum) => {
+        if(socket.room != undefined)
+            socket.leave(socket.room);
+        socket.room = roomNum;
+
+        console.log('Client joined :\n Name: ' + socket.name + '\n Room: ' + socket.room);
+        socket.join(socket.room);
+
+        io.to(socket.room).emit('joinRoom', socket.name);
     });
 });
 
