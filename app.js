a0b188c561d9cf806923c5b58e0ad32e8142f523
SD-PARK <tkdeh129@naver.com>
2022-09-22 16:41:49 +0900

    develop translate

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+const papago = require('./translate');
+
 // 포트 값 지정; localhost:{PORT}
 const PORT = 3000;
 
@@ -13,19 +15,34 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
     // 접속 시 소켓 데이터 저장
     socket.on('login', (data) => {
-        console.log('Client logged-in:\n Name: ' + data.name + '\n Userid: ' + data.userid);
+        console.log('Client logged-in:\n Name: ' + data.name + '\n Userid: ' + data.userid + '\n Languege: ' + data.languege);
         socket.name = data.name;
         socket.userid = data.userid;
+        socket.languege = data.languege;
     });
 
     // 채팅 입력 시 같은 룸의 모든 클라이언트에게 입력받은 채팅, 메세지, 시간 전송
-    socket.on('chat', (msg) => {
-        console.log('Room ' + socket.room + ' → ' + socket.name + ': ' + msg);
+    socket.on('chat', async (msg) => {
+        console.log('(Room ' + socket.room + ') ' + socket.name + ': ' + msg);
+        io.to(socket.room).emit('languegeReq');
+        let trans_msg;
+        try {
+            if(socket.languege == 'ko')
+                trans_msg = await papago.lookup('en', 'ko', msg);
+            else if(socket.languege == 'en')
+                trans_msg = await papago.lookup('ko', 'en', msg);
+        } catch(e) {
+            trans_msg = msg;
+        }
         io.to(socket.room).emit('chat', {
             name: socket.name,
-            msg: msg,
+            msg: socket.request.res.languege,
             time: timePrint()
         });
+    });
+
+    socket.on('languegeRes', () {
+        
     });
 
     // 방 입장 시 해당 룸에 접속 알림 메시지 전송
@@ -36,10 +53,16 @@ io.on('connection', (socket) => {
         }
         socket.room = roomNum;
 
-        console.log('Client joined :\n Name: ' + socket.name + '\n Room: ' + socket.room);
+        console.log('Room joined :\n Name: ' + socket.name + '\n Room: ' + socket.room);
         socket.join(socket.room);
 
         io.to(socket.room).emit('joinRoom', socket.name);
+    });
+
+    // 사용 언어 변경
+    socket.on('setLanguege', (lang) => {
+        socket.languege = lang;
+        console.log('Change Languege :\n Name: ' + socket.name + '\n Languege: ' + socket.languege);
     });
 
     /** 현재 시간 값 {am/pm hh:mm} */
