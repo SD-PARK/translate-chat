23c5c37a787e5c60462dacf0e5a8039ae435c5c4
SD-PARK <tkdeh129@naver.com>
2022-11-01 18:51:17 +0900

    mariaDB 제거

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express(); 
 const server = require('http').createServer(app);
+const nunjucks = require('nunjucks');
+
 const PORT = 3000;
 
-app.use(express.static('public'));
+app.use(express.static('public')); // root 지정
+app.use(express.urlencoded({extended: false})); // query-string
+
+// testing...
+nunjucks.configure('views', {express: app}); // views 폴더가 넌적스 파일의 위치가 됨, express 속성에 app 객체(express();) 연결
 
 app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/html/cookie-test.html');
+    res.setHeader('Set-Cookie', 'test=1');
+});
+
+// login
+app.get('/login', (req, res) => {
     res.sendFile(__dirname + '/public/html/login.html');
 });
 
